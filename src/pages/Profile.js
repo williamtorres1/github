8e9d9c1dc99f94ffab38a3f2d1a717e1e5a35fc2
williamtorres1 <williamtorres074@gmail.com>
2020-02-28 16:36:21 -0300

    borderWidth atualized

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -76,7 +76,7 @@ const styles = StyleSheet.create({
         width: 100,
         height: 100,
         borderRadius: 10,
-        borderWidth: 2,
+        borderWidth: 1,
         borderColor: '#CCCED0',
     },
     name:{
